6277e26f090bf95de62d326e5904a3ad31ff9f96
Alif Aiman <englios656@gmail.com>
2023-08-14 11:04:25 +0800

    Exercise 1 all Done,Next is E2

--- Alif/pcost.py
+++ Alif/pcost.py
@@ -17,22 +17,22 @@ def portfolio_cost(filename):
     with open (filename,'r') as f:
         totalsum=0
         for line in f:
+            fields = line.split()
             try:
-                fields = line.split()
                 nshare=int(fields[1])
                 price=float(fields[2])
                 share_price=nshare*price
                 print(f'{fields[0]:<10s}{nshare:>10d}{price:>10.2f}{share_price:>10.2f}')
                 totalsum+=share_price
                 
-            except ValueError:
-                print('Bad line',line,end='')
+            except ValueError as e:
+                print('\nBad line',line,end='')
+                print('Reason',e,end='\n\n')
                 continue
-            
-    print('\nTotal sum of all prices',float(totalsum),end='\n\n')
-    return totalsum
+    print('-'*50)       
+    print('\nTotal sum of all prices',round(float(totalsum),2),end='\n\n')
+    return round(totalsum,2)
     
-print(portfolio_cost('Data/portfolio3.dat'))
         
 
         
